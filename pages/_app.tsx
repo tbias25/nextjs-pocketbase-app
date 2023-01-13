366547c2e0d1818c45397d7aa283d6cd0d335a10
Tobias <55360690+tbias25@users.noreply.github.com>
2023-01-13 21:37:51 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     setLoggedIn(client.authStore.isValid);
     setLoading(false);
+    console.log("isLoggedIn", isLoggedIn);
   }, [client, isLoading, isLoggedIn]);
 
   if (isLoading) {
